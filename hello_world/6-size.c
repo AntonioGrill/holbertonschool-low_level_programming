768303aedd9af2c721c792e60c6d990ef974a55e
AntonioGrill <josegrillo04.99@gmail.com>
2023-08-06 01:06:25 -0700

    nuevo archivo

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	prinf("%zu bytes\n", sizeof(char));
+	prinf("%zu bytes\n", sizeof(int));
+	prinf("%zu byte(s)\n", sizeof(long int));
+	prinf("%zu byte(s)\n", sizeof(long long int));
+	prinf("%zu byte(s)\n", sizeof(float));
+	
+	return (0);
+}
